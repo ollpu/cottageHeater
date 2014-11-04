383b3be7431c63fefa0299bae6cd8b38ff59ad4a
Roope Salmi <rpsalmi@gmail.com>
2014-11-04 21:22:27 +0200

    Add EEPROM read&write functionality
    And it loads them automatically on setup()

--- cottageHeater.ino
+++ cottageHeater.ino
@@ -34,6 +34,7 @@ void setup() {
       delay(1000);
     }
   }
+  loadStatus();
   
 }
 
@@ -44,10 +45,80 @@ void loop() {
 
 //EEPROM save&load
 //Writes unit status to EEPROM
+//tempvars to check for change - to preserve the EEPROMs life
+boolean idleOld = false;
+byte idleTrgOld = 0;
+boolean targetOld = false;
+byte targetTrgOld = 0;
+boolean warmOld = false;
+byte warmWaitOld = 0;
+byte warmTrgOld = 0;
+byte warmTimeOld = 0;
+
+//EEPROM address mapping:
+//0: idle (binary)
+//1: idleTrg (byte)
+//2: target (binary)
+//3: targetTrg (byte)
+//4: warm (binary)
+//5: warmWait (byte)
+//6: warmTrg (byte)
+//7: warmTime (byte)
+
 void writeStatus() {
-  
+  //Sorry for the repetition here, but I find it's the most efficient (runtime-wise).
+  if (idle != idleOld) {
+    idleOld = idle;
+    EEPROM.write(0, idle);
+  }
+  if (idleTrg != idleTrgOld) {
+    idleTrgOld = idleTrg;
+    EEPROM.write(1, idleTrg);
+  }
+  if (target != targetOld) {
+    targetOld = target;
+    EEPROM.write(2, target);
+  }
+  if (targetTrg != targetTrgOld) {
+    targetTrgOld = targetTrg;
+    EEPROM.write(3, targetTrg);
+  }
+  if (warm != warmOld) {
+    warmOld = warm;
+    EEPROM.write(4, warm);
+  }
+  if (warmWait != warmWaitOld) {
+    warmWaitOld = warmWait;
+    EEPROM.write(5, warmWait);
+  }
+  if (warmTrg != warmTrgOld) {
+    warmTrgOld = warmTrg;
+    EEPROM.write(6, warmTrg);
+  }
+  if (warmTime != warmTimeOld) {
+    warmTimeOld = warmTime;
+    EEPROM.write(7, warmTime);
+  }
 }
+
 //Loads unit status from EEPROM
 void loadStatus() {
-  
+  idle = EEPROM.read(0);
+  idleOld = idle;
+  idleTrg = EEPROM.read(1);
+  idleTrgOld = idleTrg;
+  target = EEPROM.read(2);
+  targetOld = target;
+  targetTrg = EEPROM.read(3);
+  targetTrgOld = targetTrg;
+  warm = EEPROM.read(4);
+  warmOld = warm;
+  warmWait = EEPROM.read(5);
+  warmWaitOld = warmWait;
+  warmTrg = EEPROM.read(6);
+  warmTrgOld = warmTrg;
+  warmTime = EEPROM.read(7);
+  warmTimeOld = warmTime;
 }
+
+
